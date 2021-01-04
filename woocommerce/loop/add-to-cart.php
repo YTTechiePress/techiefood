4287618de1a6f2acc3f2732b82956c1ac62178fc
Laurence bahiirwa <laurence@mediauganda.com>
2021-01-04 15:40:04 +0300

    Add variations to theme

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -20,29 +20,70 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $product;
+global $woocommerce;
+
 // var_dump($product);
+$variation_ids = array();
+$target_attribute = 'sizes';
 
 if ( 'variable' === $product->get_type() ) {
-
-	echo '<!-- Button trigger modal -->
-	<a type="button" class="btn btn-primary" data-toggle="modal" data-target="#' . $product->slug . '"><i class="fas fa-plus-circle"></i></a>
+	?>
+	<!-- Button trigger modal -->
+	<a type="button" class="btn btn-primary" data-toggle="modal" data-target="#<?php echo $product->slug; ?>"><i class="fas fa-plus-circle"></i></a>
 	
 	<!-- Modal -->
-	<div class="modal fade" id="' . $product->slug . '" tabindex="-1" role="dialog" aria-labelledby="' . $product->slug . 'Label" aria-hidden="true">
+	<div class="modal fade" id="<?php echo $product->slug; ?>" tabindex="-1" role="dialog" aria-labelledby="<?php echo $product->slug; ?>Label" aria-hidden="true">
 	  <div class="modal-dialog" role="document">
 		<div class="modal-content">
 		  <div class="modal-header">
-			<h5 class="modal-title" id="' . $product->slug . 'Label">Modal title</h5>
+			<h5 class="modal-title" id="<?php echo $product->slug; ?>Label">Modal title</h5>
 			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 			  <span aria-hidden="true">&times;</span>
 			</button>
 		  </div>
 		  <div class="modal-body">
-			...
+			<?php
+				$default_attributes   = $product->get_default_attributes();
+				$available_attributes = $product->get_available_variations();
+
+				// var_dump($available_attributes);
+				foreach( $available_attributes as $variation_values ) {
+					foreach($variation_values['attributes'] as $key => $attribute_value ) {
+						$attribute_name = str_replace( 'attribute_', '', $key );
+						if ( $attribute_name == $target_attribute ) {
+							$default_value = $product->get_variation_default_attribute( $attribute_name );
+							// if ( $default_value == $attribute_value ) {
+								$variation_ids[] = $variation_values['variation_id'];
+							// }
+							// var_dump($variation_ids);
+						}
+					}
+				}
+
+				if ( count( $variation_ids ) > 0 ) {
+					foreach( $variation_ids as $variation_id ) {
+						$variation = wc_get_product( $variation_id );
+
+						$variation_attribute = $variation->get_variation_attributes();
+						$variation_price = $variation->get_price();
+
+						$full_name = $variation_attribute['attribute_sizes'] . ' ' . $variation_attribute ['attribute_accompaniments'] . ' ' . get_woocommerce_currency_symbol() . $variation_price;
+						
+						?>
+							<input name="<?php echo $full_name?>" value="<?php echo $full_name?>" id="<?php echo $full_name?>" type="checkbox" />
+							<label for="<?php echo $full_name?>"><?php echo $full_name?></label><br />
+
+						<?php
+						// var_dump( $variation_attribute );
+					}
+				}
+				
+			?>
 		  </div>
 		</div>
 	  </div>
-	</div>';
+	</div>
+	<?php
 } else {
 	echo apply_filters(
 		'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
